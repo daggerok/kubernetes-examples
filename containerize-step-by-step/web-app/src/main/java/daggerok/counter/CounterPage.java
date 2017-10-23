ef7bf8d5789399ee1603f0c1d75dac53c2b8a61f
Maksim Kostromin <daggerok@gmail.com>
2017-10-23 08:13:26 +0300

    update web-app - add host addr / name; add k8s web-app rc

--- containerize-step-by-step/web-app/src/main/java/daggerok/counter/CounterPage.java
+++ containerize-step-by-step/web-app/src/main/java/daggerok/counter/CounterPage.java
@@ -1,11 +1,14 @@
 package daggerok.counter;
 
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.val;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.net.Inet4Address;
 
 @Controller
 @RequiredArgsConstructor
@@ -19,6 +22,14 @@ public class CounterPage {
     val counter = repository.findOrCreate("counter");
     counter.setTotal(counter.getTotal() + 1);
     model.addAttribute("counter", repository.save(counter));
+    setProps(model);
     return "index";
+  }
+
+  @SneakyThrows
+  private static void setProps(final Model model) {
+    val host = Inet4Address.getLocalHost();
+    model.addAttribute("host", host.getHostName());
+    model.addAttribute("addr", host.getHostAddress());
   }
 }
